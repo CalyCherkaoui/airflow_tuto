5cd53e021221696fb5d75a71c07fc49bcbc17986
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2025-02-04 20:43:33 +0100

    update bash script

--- download_rocket.sh
+++ download_rocket.sh
@@ -3,16 +3,21 @@
 # initializing the Airflow database, creating an admin user,
 # and then starting the webserver and scheduler.
 
+set -x
+
+SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+
 docker run \
-  -ti \
-  -p 8080:8080 \
-  -v /path/to/dag/download_rocket_launches.py:/opt/airflow/dags/download_rocket_launches.py \
-  --entrypoint=/bin/bash \
-  --name airflow \
-  apache/airflow:2.0.0-python3.8 \
-  -c '(
-    airflow db init && \
-    airflow users create --username admin --password admin --firstname Anonymous --lastname Admin --role Admin --email admin@example.org
-  ); \
-  airflow webserver & \
-  airflow scheduler'
+-ti \
+-p 8080:8080 \
+-v ${SCRIPT_DIR}/../dags/download_rocket_launches.py:/opt/airflow/dags/download_rocket_launches.py \
+--name airflow
+--entrypoint=/bin/bash \
+apache/airflow:2.0.0-python3.8 \
+-c '( \
+airflow db init && \
+airflow users create --username admin --password admin --firstname Anonymous --lastname Admin --role Admin --email admin@example.org \
+); \
+airflow webserver & \
+airflow scheduler \
+'
